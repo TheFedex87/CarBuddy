acd1288d25b18607f75d4765b5fe63d21a66b9f6
Federico Creti <f.creti@smre.it>
2019-03-29 17:27:56 +0100

    First commit

---
+++ app/src/main/java/it/bytener/carbuddy/ui/MainFragment.java
@@ -0,0 +1,26 @@
+package it.bytener.carbuddy.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import it.bytener.carbuddy.R;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+
+    }
+}
